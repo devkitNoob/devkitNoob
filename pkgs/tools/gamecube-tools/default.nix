426dcced1cf86dd914f4b3bbd8a3b885ee0e5478
novenary <novenary@kwak.zip>
2024-09-21 22:30:24 +0300

    gamecube-tools: unstable-2024-09-09 -> unstable-2024-09-11

--- pkgs/tools/gamecube-tools/default.nix
+++ pkgs/tools/gamecube-tools/default.nix
@@ -8,7 +8,7 @@
   libGL,
 }:
 stdenv.mkDerivation (finalAttrs: {
-  version = "unstable-2024-09-09";
+  version = "unstable-2024-09-11";
   pname = "gamecube-tools";
 
   nativeBuildInputs = [autoreconfHook];
@@ -28,8 +28,8 @@ stdenv.mkDerivation (finalAttrs: {
     owner = "extremscorner";
     repo = "gamecube-tools";
     #rev = "v${finalAttrs.version}";
-    rev = "2eecc969b974535746c61bf948b7e00cf93dbc18";
-    hash = "sha256-hTJh9Jgk9bI6VrQUPBNgHOwCaf8Q37uhcPw8Jr0xdAU=";
+    rev = "4a331730e645540bd0c3e6ec284dbc4753acbba4";
+    hash = "sha256-LgR1fDcb7KV1fKHtso3Zokr9TJ6gSV9xT/1voCA2xnk=";
   };
 
   meta = {
