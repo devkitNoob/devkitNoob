447cdf67139fcb380bb182b40877d9fffa53e93f
Dusk Banks <me@bb010g.com>
2024-10-27 04:29:41 -0700

    maintainer-list: move to by-name structure

--- by-name/default/lib-overlay.nix
+++ by-name/default/lib-overlay.nix
@@ -6,5 +6,5 @@ in {
     // builtins.mapAttrs (handle: value:
       if prevMaintainers ? ${handle} && prevMaintainers.${handle} != handle
       then finalLib.warn "${handle} (${value.name}) is already a differing maintainer (${prevMaintainers.${handle}.name})" prevMaintainers.${handle}
-      else value) (import ../../pkgs/maintainer-list.nix);
+      else value) (import ./_maintainer-list.nix);
 }
