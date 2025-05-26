e1e60e88b5b02d80187a62c9dad6f3f831eb51c1
novenary <novenary@kwak.zip>
2025-05-27 00:39:29 +0300

    noobkitARM: r65 -> r66

--- pkgs/toolchains/default.nix
+++ pkgs/toolchains/default.nix
@@ -6,23 +6,23 @@
 in {
   noobkitARM = mkToolchain rec {
     pname = "noobkitARM";
-    version = "65";
+    version = "66";
     srcs = {
       buildscripts = {
         rev = "devkitARM_r${version}";
-        hash = "sha256-HBZX+lEw76GXA05GdjCWMaE/kqO8YJV55rOkVbNyxeQ=";
+        hash = "sha256-Y8J8Es431nhQlZ1k8GbV6yNsn+/pMUOGSrlXVrVqzAs=";
       };
       binutils = {
-        version = "2.43.1";
-        hash = "sha256-vsqsXSleA3WHtjpC+tV/49nXuD9HjrJLZ/nuxdDxhy8=";
+        version = "2.44";
+        hash = "sha256-9mOQpmH6oRfQD6suec8tydCXtCzClr8/hnfR57RS3Do=";
       };
       gcc = {
-        version = "14.2.0";
-        hash = "sha256-p7Obxpy/niWCbFpgqyZHcAH3wI2FzsBLwOKcq+1vPMk=";
+        version = "15.1.0";
+        hash = "sha256-4rCewhZg8B/s/7cV4BICZSFpQ/A40OSKmGhxPlTwbOo=";
       };
       newlib = {
-        version = "4.4.0.20231231";
-        hash = "sha256-DBZqOeG/CVHfr81olJ/g5LbTZYCB1igvOa7vxjEPLxM=";
+        version = "4.5.0.20241231";
+        hash = "sha256-M/EmBeAFSWWZbCXBOCs+RjsK+ReZAB9buMBjDy7IyFI=";
       };
     };
     variant = 1;
