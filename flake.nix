ac671566a046a3dfd4fe175524f07b36698932dd
novenary <novenary@kwak.zip>
2025-10-17 14:40:57 +0300

    flake: remove hydraJobs

--- flake.nix
+++ flake.nix
@@ -11,24 +11,11 @@
     inherit
       (lib)
       dontRecurseIntoAttrs
-      filterAttrs
       genAttrs
-      isAttrs
-      isDerivation
-      mapAttrs
-      recurseIntoAttrs
       ;
 
     forAllSystems = genAttrs systems;
     systems = lib.systems.flakeExposed;
-    getDerivations = attrs: let
-      sets = filterAttrs (k: v: isDerivation v || (isAttrs v && v.recurseForDerivations or false)) attrs;
-    in
-      mapAttrs (k: v:
-        if isDerivation v
-        then v
-        else getDerivations v)
-      sets;
   in {
     formatter = forAllSystems (system: let
       legacyPackages' = self.legacyPackages.${system};
@@ -45,19 +32,6 @@
       });
     in
       {inherit nixpkgs;} // nixpkgs.devkitNoob);
-
-    hydraJobs = let
-      packages = recurseIntoAttrs {
-        inherit
-          (self.legacyPackages)
-          x86_64-linux
-          aarch64-linux
-          ;
-        # FIXME Darwin builders pls 🥺
-      };
-    in {
-      packages = getDerivations packages;
-    };
 
     overlays.default = import ./overlay.nix;
   };
